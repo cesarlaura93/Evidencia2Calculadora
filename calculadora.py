5b615ac90756d2b0e4329c929c119982d0a775c3
cesar laura <clauram@ulasalle.edu.pe>
2022-04-29 13:19:46 -0500

    [cesar][added multiply operation]

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+# Funcion para multiplicar dos numeros
+def multiply(num1, num2):
+    return num1 * num2
+
+elif select == 3:
+    print(number_1, "*", number_2, "=",
+                    multiply(number_1, number_2))
+
+else:
+    print("Invalid input")
